e619a28c704597107cdf7b77c5b14788cd77ac81
John (Jef) Fisher <Fishe545@gmail.com>
2016-10-24 09:59:20 -0700

    v0.1 done, but messy AF

--- script.js
+++ script.js
@@ -12,18 +12,26 @@ var student_array = [];
  * @type {string[]}
  */
 // var inputIds = ['#studentName', '#course', '#studentGrade'];
-var inputIds = [$('#studentName'), $('#course'), $('#studentGrade')];
-var studentInputs;
+var inputIds;
 /**
  * addClicked - Event Handler when user clicks the add button
  */
 function addClicked() {
     $('.btn-success').click(function() {
         console.log('addClicked works');
-        var name = inputIds[0].val();
-        var course = inputIds[1].val();
-        var grade = inputIds[2].val();
+        // var name = $(inputIds[0]).val();
+        // var course = $(inputIds[1]).val();
+        // var grade = $(inputIds[2]).val();
+        inputIds = {
+            name: $("#studentName"),
+            course: $("#course"),
+            grade: $("#studentGrade")
+        };
+        var name = inputIds.name.val();
+        var course = inputIds.course.val();
+        var grade = inputIds.grade.val();
         addStudent(name, course, grade);
+        updateData();
     });
 }
 /**
@@ -53,7 +61,9 @@ function addStudent(theName, theCourse, theGrade) {
  * clearAddStudentForm - clears out the form values based on inputIds variable
  */
 function clearAddStudentForm() {
-
+    inputIds.name.val('');
+    inputIds.course.val('');
+    inputIds.grade.val('');
 }
 /**
  * calculateAverage - loop through the global student array and calculate average grade and return that value
@@ -61,9 +71,9 @@ function clearAddStudentForm() {
  */
 function calculateAverage() {
     console.log('calculateAverage function called');
-    var gradesTotal = null;
+    var gradesTotal = 0;
     for(var i = 0; i < student_array.length; i++) {
-        gradesTotal += parseInt(student_array[i].grade.val());
+        gradesTotal += parseInt(student_array[i].grade);
     }
     var averageGrade = gradesTotal / student_array.length;
     return $('.avgGrade').text(averageGrade);
@@ -73,12 +83,21 @@ function calculateAverage() {
  */
 function updateData() {
     updateStudentList();
+    calculateAverage();
 }
 /**
  * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
  */
 function updateStudentList() {
-
+    console.log("updateStudentList function called");
+    for(var i = 0; i < student_array.length; i++) {
+        addStudentToDom(student_array[i]);
+        // $('tbody').append('<tr>');
+        // $('tbody tr:last').append($('<td>' + student_array[i].name + '</td>'));
+        // $('tbody tr:last').append($('<td>' + student_array[i].course + '</td>'));
+        // $('tbody tr:last').append($('<td>' + student_array[i].grade + '</td>'));
+        // $('tbody tr:last').append($('<button>').addClass('btn btn-danger').text('Delete'));
+    }
 }
 /**
  * addStudentToDom - take in a student object, create html elements from the values and then append the elements
@@ -86,13 +105,20 @@ function updateStudentList() {
  * @param studentObj
  */
 function addStudentToDom(studentObj) {
-
+    $('tbody').append('<tr>');
+    $('tbody tr:last').append($('<td>' + studentObj.name + '</td>'));
+    $('tbody tr:last').append($('<td>' + studentObj.course + '</td>'));
+    $('tbody tr:last').append($('<td>' + studentObj.grade + '</td>'));
+    $('tbody tr:last').append($('<button>').addClass('btn btn-danger').text('Delete'));
 }
 /**
  * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
  */
 function reset() {
-
+    student_array = [];
+    clearAddStudentForm();
+    $('tbody').empty();
+    $('.avgGrade').text(0);
 }
 /**
  * Listen for the document to load and reset the data to the initial state
@@ -100,10 +126,9 @@ function reset() {
 $(document).ready(function () {
     addClicked();
     cancelClicked();
-    studentInputs = {
-        name: $("#studentName"),
-        course: $("#course"),
-        grade: $("#studentGrade")
-    };
-    calculateAverage();
+    // inputIds = {
+    //     name: $("#studentName"),
+    //     course: $("#course"),
+    //     grade: $("#studentGrade")
+    // };
 });
