a911b17aa58c04bcb8caff4a2c0348528d56da6d
John (Jef) Fisher <Fishe545@gmail.com>
2016-10-25 23:45:16 -0700

    still chippin away

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var studentObj = {};
  * @type {Array}
  */
 var student_array = [];
+var globalStudentDataArray = [];
 /**
  * inputIds - id's of the elements that are used to add students
  * @type {string[]}
@@ -16,7 +17,7 @@ var inputIds;
  * addClicked - Event Handler when user clicks the add button
  */
 function addClicked() {
-    $('.btn-success').click(function() {
+    $('.btn-success').click(function () {
         console.log('addClicked works');
         var name = inputIds.name.val();
         var course = inputIds.course.val();
@@ -31,7 +32,7 @@ function addClicked() {
  * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
  */
 function cancelClicked() {
-    $('.btn-default').click(function() {
+    $('.btn-default').click(function () {
         console.log('cancelClicked works');
         clearAddStudentForm();
     });
@@ -65,7 +66,7 @@ function clearAddStudentForm() {
 function calculateAverage() {
     console.log('calculateAverage function called');
     var gradesTotal = 0;
-    for(var i = 0; i < student_array.length; i++) {
+    for (var i = 0; i < student_array.length; i++) {
         gradesTotal += parseInt(student_array[i].grade);
     }
     var averageGrade = Math.round(gradesTotal / student_array.length);
@@ -85,21 +86,24 @@ function updateData() {
 function updateStudentList() {
     console.log("updateStudentList function called");
     $('.student-list.table > tbody').empty();
-    for(var i = 0; i < student_array.length; i++) {
-        addStudentToDom(student_array[i],i);
+    var globalData = 1; //set globalData var equal to the id of the object at index 0 of globalStudentDataArray
+    for (var i = 0; i < student_array.length; globalData++, i++) {
+        addStudentToDom(globalStudentDataArray[i], i);
+        addStudentToDom(student_array[i], i + globalStudentDataArray.length);
     }
 }
 /**
  * addStudentToDom - take in a student object, create html elements from the values and then append the elements
  * into the .student_list tbody
  * @param studentObj
+ * @param number
  */
-function addStudentToDom(studentObj,number) {
-    $('tbody').append('<tr>');
-    $('tbody tr:last').append($('<td>' + studentObj.name + '</td>'));
-    $('tbody tr:last').append($('<td>' + studentObj.course + '</td>'));
-    $('tbody tr:last').append($('<td>' + studentObj.grade + '</td>'));
-    $('tbody tr:last').append($('<button>').addClass('btn btn-danger button-index'+number).text('Delete'));
+function addStudentToDom(studentObj, number) {                               //function that adds student to DOM with 2 params: studentObj and number
+    $('tbody').append('<tr>');                                              //creates and appends rows to table body
+    $('tbody tr:last').append($('<td>' + studentObj.name + '</td>'));       //creates and appends name to table data
+    $('tbody tr:last').append($('<td>' + studentObj.course + '</td>'));     //creates and appends course to table data
+    $('tbody tr:last').append($('<td>' + studentObj.grade + '</td>'));      //creates and appends grade to table data
+    $('tbody tr:last').append($('<button>').addClass('btn btn-danger button-index' + number).text('Delete')); //creates and appends grade to table data
 }
 /**
  * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
@@ -112,26 +116,75 @@ function reset() {
 
 }
 /**
- * removeStudent - delete button click handler that removes the deleted student's object from the DOM and from the student_array
+ * removeStudent - Event handler that removes the deleted student's object from the DOM and from the student_array
  */
 function removeStudent() {
-    $('tbody').on('click', '.btn-danger', function(){
+    $('tbody').on('click', '.btn-danger', function () {
         var indexPosition = $(this).closest('tr').index();
         $(this).closest('tr').remove();
-        student_array.splice(indexPosition,1);
+        student_array.splice(indexPosition, 1);
     })
 }
 
+
+function addIdentifyingNumber() {
+    var identifyingNumber = 1;
+    for (; identifyingNumber < globalStudentDataArray.length; identifyingNumber++) {
+        globalStudentDataArray.push(response.data[0].attr('id', identifyingNumber));
+    }
+}
 /**
- * Listen for the document to load and reset the data to the initial state
+ * getOverHereData - Event handler that gets data from the database using an AJAX call and pushes it into the globalStudentDataArray
  */
-$(document).ready(function () {
-    addClicked();
-    cancelClicked();
-    removeStudent();
-    inputIds = {
-        name: $("#studentName"),
-        course: $("#course"),
-        grade: $("#studentGrade")
-    };
-});
+function getOverHereData() {
+    $('.btn-warning').click(function () {
+        var data_object = {
+            api_key: 'LrLCpNcjb5',
+            name: 'Jef'
+        };
+        console.log('made it to ajax call');
+        $.ajax({
+            data: data_object,
+            dataType: 'json',
+            method: 'post',
+            url: 'https://s-apis.learningfuze.com/sgt/get',
+            success: function (response) {
+                console.log('success of ajax call', response.data);
+                addIdentifyingNumber();
+
+            }
+        })
+    });
+
+// function addShit() {
+//     for(var i=0;i<KevinArray.length; i++) {
+//         console.log("called");
+//         NewKevinArray.push(KevinArray[i].course);
+//         console.log(KevinArray[i][i].data[i].course);
+//     }
+// }
+// console.log(addShit());
+//
+// KevinArray[0].data[0].course;
+//
+//
+// NewKevinArray[0].data[0].course
+    /**
+     * applyClickHandlers - function to be called in the listen that applies all click handlers
+     */
+    function applyClickHandlers() {
+        getOverHereData();
+        addClicked();
+        cancelClicked();
+        removeStudent();
+        inputIds = {
+            name: $("#studentName"),
+            course: $("#course"),
+            grade: $("#studentGrade")
+        };
+    }
+
+    /**
+     * Listen for the document to load and reset the data to the initial state
+     */
+    $(document).ready(applyClickHandlers());}
